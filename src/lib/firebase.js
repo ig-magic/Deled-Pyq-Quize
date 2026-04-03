5e56ead6f8eede86392eed5fe4e3e9c5aa54c199
Vishal <bnhh12248@gmail.com>
2026-04-03 16:02:45 +0530

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+// firebase config
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
